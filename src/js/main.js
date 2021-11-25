2a351dcb7fe5885f4e540e711fc7733622eb96fb
BaptisteDombard <91158751+BaptisteDombard@users.noreply.github.com>
2021-11-25 14:10:41 +0100

    innit

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,21 @@
-document.documentElement.classList.add("js-enabled");
+(function(){
+    const DayNight = {
+        innit(){
+            this.eBody = document.querySelector("body");
+            this.eTumbler = document.querySelector(".tumbler");
+            this.aPosts = document.querySelectorAll(".post");
+            document.documentElement.classList.add("js-enabled");
+            document.querySelector(".tumbler__wrapper").addEventListener("click",  (event) =>{
+                this.eBody.classList.toggle("body--night-mode");
+                this.eTumbler.classList.toggle("tumbler--night-mode");
+                for (const ePost of this.aPosts) {
+                    ePost.classList.toggle("post--night-mode");
+                }
+            });
+        }
+    }
+    DayNight.innit();
+})();
 
-console.log(document.querySelector("h1").textContent);
-function fToggleNightMod(event) {
 
-}
-document.querySelector(".tumbler__wrapper").addEventListener("click", fToggleNightMod);
+
