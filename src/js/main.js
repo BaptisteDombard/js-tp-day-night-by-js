5a39dbdeac64a03bdc5830b2e2dc64e56a20d6d7
BaptisteDombard <91158751+BaptisteDombard@users.noreply.github.com>
2021-11-25 12:29:27 +0100

    innit

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+document.documentElement.classList.add("js-enabled");
+
+console.log(document.querySelector("h1").textContent);
+function fToggleNightMod(event) {
+
+}
+document.querySelector(".tumbler__wrapper").addEventListener("click", fToggleNightMod);
